bb1e67c2359b4ad83628db6513251215671cc7b2
fengandzhy <fengandzhy@gmail.com>
2023-08-27 19:36:46 +1200

    react_stating_github_search basic project

--- react/react_stating_github_search/src/App.jsx
+++ react/react_stating_github_search/src/App.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import Search from "./components/search/Search";
 import List from "./components/list/List";
-class App extends React.Component{
-    state = {users:[]};
-    getUsers = (users) =>{
-        this.setState({users: users});
+
+class App extends React.Component {
+    state = {
+        users: [],
+        isFirst: true,
+        isLoading: false,
+        err: ''
+    };
+    searchUsers = (result) => {
+        this.setState(result);
     }
-    render(){
-        const {users} = this.state;
+    updateState = (stateObj) => {
+        this.setState(stateObj);
+    }
+
+    render() {
         return (
             <div className="container">
-                <Search getUsers={this.getUsers}/>
-                <List users={users}/>
+                <Search searchUsers={this.searchUsers} updateState={this.updateState}/>
+                <List state={this.state}/>
             </div>
         );
     }
 }
+
 export default App;
