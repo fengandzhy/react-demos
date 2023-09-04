2a8067df3934f176492feef2d26408e0287e84e0
fengandzhy <fengandzhy@gmail.com>
2023-09-04 15:50:19 +1200

    react_stating_spring_boot basic project

---
+++ react/react_stating_router/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Search from "./components/search/Search";
+import List from "./components/list/List";
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+
+export default App;
