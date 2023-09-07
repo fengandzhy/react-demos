1e5f0daf70cca553e2785b22ae0880e9844537b2
fengandzhy <fengandzhy@gmail.com>
2023-09-07 20:36:39 +1200

    react_stating_route basic project

---
+++ react/react_stating_router_nav_link/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import About from './pages/about/About'
+import Home from './pages/home/Home'
+import {NavLink, Route} from 'react-router-dom'
+import './App.css';
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <div className="page-header"><h2>React Router Demo</h2></div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+                            <NavLink activeClassName="frank" className="list-group-item" to="/about">About</NavLink>
+                            <NavLink activeClassName="frank" className="list-group-item" to="/home">Home</NavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                {/* 注册路由 */}
+                                <Route path="/about" component={About}/>
+                                <Route path="/home" component={Home}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ react/react_stating_router_nav_link/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <h3>我是Home的内容</h3>
+        );
+    }
+}
+
+export default Home;
